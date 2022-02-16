7967e471772ca141c430ddb28224f5777e02f897
liuxiaoming <838395826@qq.com>
2022-02-16 23:55:29 +0800

    init

---
+++ app/src/main/java/com/ming/opengl/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ming.opengl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val contain = findViewById<FrameLayout>(R.id.fl_contain)
+    }
+}
